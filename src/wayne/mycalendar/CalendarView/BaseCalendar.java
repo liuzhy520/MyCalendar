28d1dbdc680580f4bb4593d904f36c7f28c7581f
liuzhy520 <liuzhy520@gmail.com>
2015-03-06 16:46:17 +0800

    months display bug fixed

--- src/wayne/mycalendar/CalendarView/BaseCalendar.java
+++ src/wayne/mycalendar/CalendarView/BaseCalendar.java
@@ -111,8 +111,8 @@ public class BaseCalendar extends LinearLayout {
     }
     public void setToday(){
         calendarView.goToday();
-        mDate.setYear(calendarView.getYear());
-        mDate.setMonth(calendarView.getMonth() - 1900);
+        mDate.setYear(calendarView.getYear()- 1900);
+        mDate.setMonth(calendarView.getMonth() );
         mDate.setDate(calendarView.mToday.getDayOfMonth());
         tv_month.setText(DateUtils.getMonthString(calendarView.getMonth(), DateUtils.LENGTH_LONG));
     }
